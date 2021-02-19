bd6c4c8df2769764499817b92e9c8c276b0666aa
Kh. Ashiqur Rahman <ashiqur.buet16@gmail.com>
2021-02-19 17:06:17 +0600

    fetch data from local json and populate table

---
+++ issuer-frontend/js/index.js
@@ -0,0 +1,62 @@
+const BUTTON = "<a href=\"<url>\" class=\"btn btn-primary btn-sm active\" role=\"button\" aria-pressed=\"true\">View</a>";
+const IMG_PREVIEW = "<img src=\"<url>\" height=\"70\" width=\"50\">"
+
+function loadDocuments () {
+    const request = new XMLHttpRequest();
+
+    request.open("get", "../data/docs.json");
+    request.onload = () => {
+        try {
+            const json = JSON.parse(request.responseText);
+            populateTable(json);
+        }
+        catch(e) {
+            console.warn("Could not fetch data !!");
+        }
+    };
+
+    request.send();
+
+}
+
+function populateTable(json) {
+    
+    const tableBody = document.querySelector("#docs-table > tbody");
+    
+    while (tableBody.firstChild) {
+        tableBody.removeChild(tableBody.firstChild);
+    }
+
+
+    json.forEach(element => {
+        const tr = document.createElement("tr");
+        
+        const tdId= document.createElement("td");
+        tdId.textContent = element.id;
+        tdId.classList.add("align-middle");
+
+        const tdName = document.createElement("td");
+        tdName.textContent = element.name;
+        tdName.classList.add("align-middle");
+
+        const tdButton = document.createElement("td");
+        tdButton.innerHTML = BUTTON.replace("<url>", element.url);
+        tdButton.classList.add("align-middle");
+
+        const tdPreview = document.createElement("td");
+        tdPreview.innerHTML = IMG_PREVIEW.replace("<url>", element.preview);
+        tdButton.classList.add("align-middle");
+
+        tr.appendChild(tdId);
+        tr.appendChild(tdName);
+        tr.appendChild(tdButton);
+        tr.appendChild(tdPreview);
+
+        tableBody.appendChild(tr);
+    });
+    
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    loadDocuments();
+})
