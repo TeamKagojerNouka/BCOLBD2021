8ab82aee851fa7eb8fcf8192a629961a06636d97
Kh. Ashiqur Rahman <ashiqur.buet16@gmail.com>
2021-02-21 20:03:46 +0600

    add signing confirmation modal

--- issuer-frontend/js/detail.js
+++ issuer-frontend/js/detail.js
@@ -4,11 +4,38 @@ function readStorage() {
     document.getElementById("doc-img").src = obj.preview;
     document.getElementById("doc-date").textContent = obj.date;
     document.getElementById("doc-name").textContent = obj.name;
+    document.getElementById("confirmation-modal-text").textContent = "You are about to sign " + obj.name + " ID #"+ obj.id + " owned by " + obj.owner; 
     
-    
+    document.getElementById("confirmation-modal-btn").addEventListener("click", function(event){
+        animateProgressBar();
+    });
+
     console.log(obj)
 }
 
 document.addEventListener("DOMContentLoaded", () => {
     readStorage();
 })
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function animateProgressBar() {
+
+    for (let i = 0; i <= 100; i+=20) {
+
+        const pb = $('#progressBar');
+        const pbProgress = $('#progressBar span');
+        
+        
+        pbProgress.css("width", `${i}%`);
+        pb.show();
+    
+        await sleep(1000);
+    }
+    // if (progress >= 100) {
+    //     pb.hide();
+    // }
+
+}
