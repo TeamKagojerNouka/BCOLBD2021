cd99370f835e5ac57f1cdf1210b23c606542a2e1
Kh. Ashiqur Rahman <ashiqur.buet16@gmail.com>
2021-02-22 17:39:14 +0600

    refactor

--- issuer-frontend/js/upload.js
+++ issuer-frontend/js/upload.js
@@ -10,3 +10,34 @@ function readURL(input) {
         reader.readAsDataURL(input.files[0]);
     }
 }
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function animateProgressBar() {
+
+    const pb = $('#progressBar');
+
+    for (let i = 0; i <= 100; i += 20) {
+
+        const pbProgress = $('#progressBar span');
+
+        pbProgress.css("width", `${i}%`);
+        pb.show();
+
+        await sleep(1000);
+    }
+
+    pb.hide();
+    const anim = document.getElementById("confirmation-modal-anim");
+    anim.style["display"] = "flex";
+    document.getElementById("confirmation-modal-text").innerHTML = "<b>You have successfully uploaded this document.</b>";
+    document.getElementById("confirmation-modal-title").innerHTML = "<b>Upload Complete</b>";
+    document.getElementById("confirmation-modal-btn-success").style["display"] = "none";
+    document.getElementById("confirmation-modal-btn-close").textContent = "Done";
+}
+
+document.getElementById("confirmation-modal-btn-success").addEventListener("click", function(event) {
+    animateProgressBar();
+});
